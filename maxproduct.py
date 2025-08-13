d14da2c8016b3a14cbc521aac6f86e8d7d836ff5
NadimidoddiSreelatha <nadimidoddisreelatha@gmail.com>
2025-08-13 19:18:38 +0530

    Create maxproduct.py

---
+++ maxproduct.py
@@ -0,0 +1,9 @@
+l=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+subarray=len(l)
+max_product=1
+if __name__=="__main__":
+    arr=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    for i in range
+    max_product, subarray==i*(i+1)
+    print("Maximum product:", max_product)
+    print("Subarray:", subarray)
